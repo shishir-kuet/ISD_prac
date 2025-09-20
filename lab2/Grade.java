3bc453df4f1e5be49a97605c6c80bcf772a5931e
Md. Mosaddek Ali <mosaddekshishir4113@gmail.com>
2025-09-21 01:03:39 +0600

    Ayo rebase

--- lab2/Grade.java
+++ lab2/Grade.java
@@ -2,7 +2,7 @@ public class Grade {
 
     public static String gradeFromScore(int score) {
         if (score < 0 || score > 100) throw new IllegalArgumentException("score must be 0-100");
-        if (score >= 90) return "A";
+        if (score >= 90) return "A+";
         if (score >= 80) return "B";
         if (score >= 70) return "C";
         if (score >= 60) return "D";
