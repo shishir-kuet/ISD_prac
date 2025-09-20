7ff5715fa7dd529f4b80357cf60b6590314e3bdd
Md. Mosaddek Ali <mosaddekshishir4113@gmail.com>
2025-09-21 00:22:01 +0600

    Creating conflict

--- lab2/Calculator.java
+++ lab2/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 
     public double subtract(double a, double b) { return a - b; }
 
-    public double multiply(double a, double b) { return a * b; }
+    public double multiply(double a, double b) { return (a * b) + 1; }
 
     public double divide(double a, double b) {
         if (b == 0) throw new IllegalArgumentException("Division by zero not allowed");
